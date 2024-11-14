60bd2e38c651fffadd65adfe38f134ccb0987839
Daniels404 <zapatadanelstiven1710s.c@gmail.com>
2024-11-13 21:34:24 -0500

    se agrego carpeta de respositrios e interfaz con sus respectivos modulos

---
+++ SistemaCounterTex/Controllers/repositories/interfaz/IRegistro.cs
@@ -0,0 +1,12 @@
+﻿using SistemaCounterTex.Models;
+
+namespace SistemaCounterTex.Controllers.repositories.interfaz
+{
+    public interface IRegistro
+    {
+        Task<List<Registro>> GetRegistro();
+        Task<bool> PostRegistro(Registro registro);
+        Task<bool> PutRegistro(Registro registro);
+        Task<bool> DeleteRegistro(Satelite satelite);
+    }
+}
---
+++ SistemaCounterTex/Controllers/repositories/interfaz/ISatelite.cs
@@ -0,0 +1,16 @@
+﻿using SistemaCounterTex.Models;
+
+namespace SistemaCounterTex.Controllers.repositories.interfaz
+{
+    public interface ISatelite
+    {
+
+        Task<List<Satelite>> GetSatelites();
+        Task<bool> PostSatelite(Satelite satelite);
+        Task<bool> PutSatelite(Satelite satelite);
+        Task<bool> DeleteSatelite(Satelite satelite);
+
+
+    }
+
+}
