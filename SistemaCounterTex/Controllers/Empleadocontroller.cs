389fe97d5616c214edb2ed0d56fbd64cd1072d22
Daniels404 <zapatadanelstiven1710s.c@gmail.com>
2024-11-15 02:19:42 -0500

    se agrego controladores de los modulos faltantes que son empleado,provedor ,satelite,usuarios y se modificaron cosas pequeñas y errores minusculos de sintaxis.

---
+++ SistemaCounterTex/Controllers/Empleadocontroller.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaCounterTex.Data.interfaz;
+using SistemaCounterTex.Models;
+
+namespace SistemaCounterTex.Controllers
+{
+    public class Empleadocontroller : Controller
+    {
+        
+            private readonly IEmpleado _empleado;
+
+            public Empleadocontroller(IEmpleado empleado)
+            {
+                _empleado = empleado;
+            }
+
+            [HttpGet("GetAdministrador")]
+            [ProducesResponseType(StatusCodes.Status200OK)]
+            [ProducesResponseType(StatusCodes.Status400BadRequest)]
+            [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+            public async Task<IActionResult> GetEmpleado()
+            {
+                var response = await _empleado.GetEmpleado();
+                return Ok(response);
+            }
+
+            [HttpPost("PostEmpleado")]
+            [ProducesResponseType(StatusCodes.Status201Created)]
+            [ProducesResponseType(StatusCodes.Status400BadRequest)]
+            public async Task<IActionResult> PostEmpleado([FromBody] PerfilEmpleado empleado)
+            {
+                try
+                {
+                    var response = await _empleado.PostEmpleado(empleado);
+                    if (response == true)
+                        return Ok("ingreso correctamente");
+                    else
+                        return BadRequest(response);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        
+    }
+}
---
+++ SistemaCounterTex/Controllers/Provedorcontroller.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaCounterTex.Data.interfaz;
+using SistemaCounterTex.Models;
+
+namespace SistemaCounterTex.Controllers
+{
+    public class Provedorcontroller : Controller
+    {
+       
+            private readonly IProvedor _provedor;
+
+            public Provedorcontroller(IProvedor provedor)
+            {
+                _provedor = provedor;
+            }
+
+            [HttpGet("GetProveedor")]
+            [ProducesResponseType(StatusCodes.Status200OK)]
+            [ProducesResponseType(StatusCodes.Status400BadRequest)]
+            [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+            public async Task<IActionResult> GetProveedor()
+            {
+                var response = await _provedor.GetProveedor();
+                return Ok(response);
+            }
+
+            [HttpPost("PostProveedor")]
+            [ProducesResponseType(StatusCodes.Status201Created)]
+            [ProducesResponseType(StatusCodes.Status400BadRequest)]
+            public async Task<IActionResult> PostProveedor([FromBody] Proveedor provedor)
+            {
+                try
+                {
+                    var response = await _provedor.PostProveedor(provedor);
+                    if (response == true)
+                        return Ok("perfil ingresado correctamente");
+                    else
+                        return BadRequest(response);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        
+    }
+
+
+
+
+
+
+}
