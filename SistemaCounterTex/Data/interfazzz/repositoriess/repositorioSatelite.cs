319bf85711aea8e4760a5f02398273de0b2521f7
Daniels404 <zapatadanelstiven1710s.c@gmail.com>
2024-11-14 23:45:18 -0500

    se agregaron los controladores de administrador y registro

---
+++ SistemaCounterTex/Data/interfazzz/repositoriess/repositorioSatelite.cs
@@ -0,0 +1,44 @@
+﻿using SistemaCounterTex.Models;
+
+using Microsoft.EntityFrameworkCore;
+using SistemaCounterTex.Data.interfaz;
+
+namespace SistemaCounterTex.Data.repositories
+{
+    public class RepositorioSatelite : ISatelite
+    {
+        private readonly CounterTexDBContext context;
+        public RepositorioSatelite(CounterTexDBContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<Satelite>> Getsatelites()
+        {
+            var data = await context.Satelites.ToListAsync();
+            return data;
+        }
+        public async Task<bool> PostSatelite(Satelite satelite)
+        {
+            await context.Satelites.AddAsync(satelite);
+            await context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> PutSatelite(Satelite satelite)
+        {
+            context.Satelites.Update(satelite);
+            await context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> DeleteSatelite(Satelite satelite)
+        {
+            context.Satelites.Remove(satelite);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+        public Task<List<Satelite>> GetSatelites()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
